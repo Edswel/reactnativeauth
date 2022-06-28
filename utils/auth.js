7b67884ed55b377d868793ac786ca371389eaeb6
Edswel <franckini@yahoo.com>
2022-06-28 11:18:31 +0100

    add create new user functionality

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyDo5DX9jp7I37Mxx-A7hbp6VGj3jVfgmiM';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
     const response = await axios.post(
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
         {
